9ba718b071f2a25c7b9dfc71baff780adea7ffa9
yunlong <yunlong@baidu.com>
2013-12-05 16:06:10 +0800

    bae config

--- app/server_config/bae.js
+++ app/server_config/bae.js
@@ -1,12 +1,12 @@
 //config file for bae
 if(sumeru.BAE_VERSION){
   sumeru.config.database({
-    dbname : 'your_bae_db_name',
-    user: 'your_bae_ak',//bae 3.0 required
-    password: 'your_bae_sk',//bae 3.0 required
+    dbname : 'WuNPpkwjUVhYkfNsBKPa',
+    user: 'UwA5TZnx6P0ktb1RqM71dPIW',//bae 3.0 required
+    password: 'SMnMLlvs6RpETSFE7yp7u77RPGOinCKQ',//bae 3.0 required
   }); 
   sumeru.config({
-    site_url : 'http://yourapp.duapp.com/', //with tailing slash
+    site_url : 'http://gogotogether.duapp.com/', //with tailing slash
   }); 
 
 }
